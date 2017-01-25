a6e824f1c3a318c37d15b9e4316c24155c377611
MohamedA-M-Hassan <anehmr0@gmail.com>
2017-01-25 02:05:27 +0200

    start: class largeNum

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main()
+{
+    cout<< "hello";
+    return 0;
+}
+
+class LargeNumber
+{
+private:
+    vector <char> numberContainer;
+    int numberOfDigits;
+
+public:
+    LargeNumber() {}
+    void setValue(string s){}
+
+    int getValueUsingIndex(){}
+    void addNewMostSignificantDigit(int val){}
+    void changeDigit(int index,int val){}
+
+};
+

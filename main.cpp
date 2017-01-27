1b7572bfd858bc1cea3b73c39dd444f1e7cfba26
MohamedA-M-Hassan <anehmr0@gmail.com>
2017-01-27 04:01:30 +0200

    addOperation

--- main.cpp
+++ main.cpp
@@ -1,44 +1,63 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <sstream>
 using namespace std;
 
 class LargeNumber
 {
 private:
     vector <char> numberContainer;
-    //int numberOfDigits;
-
 public:
-    LargeNumber(){numberOfDigits=0;}
-    void setValue(string s);  //
+    LargeNumber(){}
+    void setValue(string s);
     int getValueUsingIndex(int index);
     void addNewMostSignificantDigit(char val);
     void changeDigit(int index,char val);
     int numberOfDigits();
+    void print();
+    LargeNumber helpAddOperation(LargeNumber x,LargeNumber y);
+    LargeNumber addOperation(LargeNumber x,LargeNumber y);
 };
+
+/*class BigNumberOperatin
+{
+public:
+    BigNumberOperatin();
+    LargeNumber helpAddOperation(LargeNumber x,LargeNumber y);
+    LargeNumber addOperation(LargeNumber x,LargeNumber y);
+};*/
 
 int main()
 {
     string m = "1234567890";
-    LargeNumber trial_;
-    trial_.setValue(m);
-    int mm=1;
-    char h= (char)mm;
-    int g=trial_.getValueUsingIndex(10);
+    LargeNumber trial1;
+    LargeNumber trial2;
+
+    trial1.setValue(m);
+    m = "9871236540";
+    trial2.setValue(m);
+    LargeNumber trial3;
+    trial3= trial3.addOperation(trial1,trial2);
+    int mm=9;
+    char h= mm+'0';
+    string sss = static_cast<ostringstream*>( &(ostringstream() << mm) )->str();
+
+    int g=trial1.getValueUsingIndex(22);
     vector <char> hi;
     for (int var = 0; var < 10; ++var) {
        hi.push_back(var);
     }
     hi.size();
-    cout<< h<< " "<<hi.size()<<endl;
+    cout<< g<< " "<<sss<<endl;
+    trial3.print();
 
     return 0;
 }
 
 
 
-// the functions
+//  LargeNumber functions implementation
 void LargeNumber::setValue(string s)
 {
     for (int i=s.size()-1;i>=0;i--)
@@ -54,7 +73,7 @@ int LargeNumber::getValueUsingIndex(int index)
         return temp;
     }
     else
-        return -1;
+        return numberContainer[index];
 }
 void LargeNumber::addNewMostSignificantDigit(char val)
 {
@@ -67,9 +86,47 @@ void LargeNumber::changeDigit(int index,char val)
 int LargeNumber::numberOfDigits(){
     return numberContainer.size();
 }
+void LargeNumber::print(){
+    for (int i=numberContainer.size()-1;i>=0;i--)
+    {
+        cout << numberContainer[i];
+    }
+}
 
-/*mySign = positive;
-myDigits.resize(1);
-myDigits[0] = '0';
-myNumDigits = 1;*/
+// add: z=x+y :: 2 functions
+// helpAddOperation fn just we call it in (addOperation) fn
+LargeNumber LargeNumber::helpAddOperation(LargeNumber x,LargeNumber y){
+    LargeNumber z;
+    int carry=0;
+    int tempInt;
+    for (int i = 0; i < y.numberOfDigits(); i++) // we use y.numberOfDigits() not y.numberOfDigits()-1  if there is a (carry) in last step
+    {
+        string tempString;
+        tempInt=x.getValueUsingIndex(i)+y.getValueUsingIndex(i)+carry;
+         if (tempInt > 9)
+         {
+              tempInt -= 10;
+              carry = 1;
+              tempString = static_cast<ostringstream*>( &(ostringstream() << tempInt) )->str();;
+              z.setValue(tempString);
+         }
+         else
+         {
+             carry = 0;
+             tempString = static_cast<ostringstream*>( &(ostringstream() << tempInt) )->str();;
+             z.setValue(tempString);
+         }
+    }
+    return z;
+}
+LargeNumber LargeNumber::addOperation(LargeNumber x,LargeNumber y){
+    LargeNumber z;
+    if (y.numberOfDigits()>=x.numberOfDigits())
+    {
+        z= helpAddOperation(x,y);
+    }
+    else z= helpAddOperation(y,x);  // x > y in digits
 
+    return z;
+
+}

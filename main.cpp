ee6e23a7f5b84fceb067b4fa9a7494170e9df5a8
boogy <anehmr0@gmail.com>
2017-03-11 07:54:01 +0200

    create constractors

--- main.cpp
+++ main.cpp
@@ -4,4 +4,45 @@
 #include <sstream>
 #include <cmath>
 using namespace std;
+class BigNum
+{
+public:
+	BigNum() {};
+	BigNum(string s);
+	BigNum(const unsigned long long  &s);
+	BigNum(vector <unsigned long long> &v);
+private:
+	vector <unsigned long long> numberContainer;
+};
+
+BigNum::BigNum(string s) {
+	string temp;
+	int counter = 0;
+	for (int i = s.size() - 1;i >= 0;i = i - 9)
+	{
+
+		if (i >= 9) {
+			temp = s.substr(i - 8, 9);
+			numberContainer.push_back(stoull(temp));
+			counter++;
+		}
+		else {
+			temp = s.substr(0, s.size() - counter * 9);
+			numberContainer.push_back(stoull(temp));
+		}
+	}
+
+}
+BigNum::BigNum(const unsigned long long &s) {
+	if (s <= 999999999)
+		numberContainer.push_back(s);
+	else
+	{
+		numberContainer.push_back(s % 1000000000);
+		numberContainer.push_back((s - numberContainer[0]) / 1000000000);
+	}
+}
+BigNum::BigNum(vector <unsigned long long> &v) {
+	numberContainer = v;
+}
 

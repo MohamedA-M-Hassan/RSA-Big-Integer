7ff8ce2827b0eaa25418d9b779fd5bd5f3d110e8
boogy <anehmr0@gmail.com>
2017-03-11 07:55:31 +0200

    set value

--- main.cpp
+++ main.cpp
@@ -11,6 +11,10 @@ public:
 	BigNum(string s);
 	BigNum(const unsigned long long  &s);
 	BigNum(vector <unsigned long long> &v);
+	void setValue(string s);//in case pass ( string ): in the initial state
+	void setValue(unsigned long long s);//  in case pass (number) to use it in the operation
+	void setValueUsingVector(vector <unsigned long long> n);
+	
 private:
 	vector <unsigned long long> numberContainer;
 };
@@ -46,3 +50,25 @@ BigNum::BigNum(vector <unsigned long long> &v) {
 	numberContainer = v;
 }
 
+void BigNum::setValue(string s) {
+	string temp;
+	int counter = 0;
+	for (int i = s.size() - 1;i >= 0;i = i - 9)
+	{
+
+		if (i >= 9) {
+			temp = s.substr(i - 8, 9);
+			numberContainer.push_back(stoull(temp));
+			counter++;
+		}
+		else {
+			temp = s.substr(0, s.size() - counter * 9);
+			numberContainer.push_back(stoull(temp));
+		}
+	}
+}
+void BigNum::setValue(unsigned long long s)
+{
+	numberContainer.push_back(s);
+}
+void BigNum::setValueUsingVector(vector <unsigned long long> n) { numberContainer = n; }
